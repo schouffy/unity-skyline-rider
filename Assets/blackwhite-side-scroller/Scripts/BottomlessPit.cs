72a9c0b2c6fbf33aa5e8f451293ad69f11455ce9
Jérôme Scheefer <jerome.scheefer@gmail.com>
2020-06-08 12:21:56 +0200

    Creating first level
    Reorganizing assets, handling bottomless pits

---
+++ Assets/blackwhite-side-scroller/Scripts/BottomlessPit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class BottomlessPit : MonoBehaviour
+{
+    private Collider2D _trigger;
+    public UnityEvent OnPlayerEnter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _trigger = GetComponent<Collider2D>();
+        if (!_trigger.isTrigger)
+            Debug.LogError($"{name} collider should be a trigger");
+
+        if (OnPlayerEnter == null)
+            OnPlayerEnter = new UnityEvent();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // stop camera follow
+            Camera.main.GetComponent<SmoothFollow2D>().enabled = false;
+
+            // TODO play fall and crash sounds synchronized with fade to black
+
+            OnPlayerEnter.Invoke();
+        }
+    }
+
+}
---
+++ Assets/blackwhite-side-scroller/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator UIAnimator;
+    public float TransitionTime = 1f;
+
+    public void RestartCurrentLevel()
+    {
+        StartCoroutine(Reload());
+    }
+
+    private IEnumerator Reload()
+    {
+        UIAnimator.SetTrigger("Start");
+
+        // Leave time for fade to black to happen
+        yield return new WaitForSeconds(TransitionTime);
+
+        // reload scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
