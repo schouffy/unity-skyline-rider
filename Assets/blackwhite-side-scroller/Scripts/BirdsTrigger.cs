65bceea530e9285fcb41c509f0671c376b74302e
Jérôme Scheefer <jerome.scheefer@gmail.com>
2020-06-30 17:41:35 +0200

    Added some birds and fixed some issues

---
+++ Assets/blackwhite-side-scroller/Scripts/BirdsTrigger.cs
@@ -0,0 +1,25 @@
+﻿using Assets.blackwhite_side_scroller.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BirdsTrigger : GameTrigger
+{
+    public GameObject BirdsOnGround;
+    public ParticleSystem BirdsParticleSystem;
+    private bool _alreadyFlownAway;
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_alreadyFlownAway)
+            return;
+
+        if (collision.gameObject.tag == Constants.TagPlayer)
+        {
+            BirdsParticleSystem.Play();
+            BirdsOnGround.SetActive(false);
+            _alreadyFlownAway = true;
+        }
+    }
+}
